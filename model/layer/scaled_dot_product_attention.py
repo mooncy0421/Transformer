c97876ca44891aca9b78b6c62288520ff5db1116
mooncy0421 <co1504@naver.com>
2021-08-12 22:13:28 +0900

    FEAT: Complete 'forward' method
    Completed 'forward' method in class 'ScaledDotProductAttention'.
    Class for perform Scaled Dot-prouct Attention for Multi-Head attention
    in Transformer architecture.

--- model/layer/scaled_dot_product_attention.py
+++ model/layer/scaled_dot_product_attention.py
@@ -1,6 +1,6 @@
 """
 Author : Chiyeong Heo
-Date : 2021-08-11
+Date : 2021-08-12
 """
 
 import math
@@ -11,5 +11,21 @@ class ScaledDotProductAttention(nn.Module):
         super(ScaledDotProductAttention, self).__init__()
         self.softmax = nn.Softmax()
 
-    def forward(self, Q, K, V, mask=None, e=1e-12):
+    def forward(self, Q, K, V, mask=None):
+        # For Multi-Head + Batch => 4D Tensor (Q,K,V)
+        # [batch_size, num_haeds, seq_len, d_vector]
+        batch_size, num_heads, seq_len, d_vector = K.size()
 
+        K_T = K.view(batch_size, num_heads, d_vector, seq_len)      # Transpose K
+        attn_weight = (Q @ K_T) / math.sqrt(d_vector)        # Scaled Dot-Product
+
+        # Masking
+        if mask is not None:
+            attn_weight = attn_weight.masked_fill(mask==0, -1e-12)
+
+        attn_weight = self.softmax(attn_weight)       # [0,1] range
+
+        V = attn_weight @ V     # Weighted Sum of Value
+
+        return V, attn_weight
+        

6523c0d136666bbb89b2ea9c0b44c8160ed8160d
mooncy0421 <co1504@naver.com>
2021-08-22 23:44:30 +0900

    FEAT: Add value for weight decay and initializing learning rate
    Add value for optimizer hyperparameter. 'init_lr' and 'weight_decay' for
    initializing learning rate and rate for weight decay.

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 """
 Author : Chiyeong Heo
 Date : 2021-08-17
+Reference : https://github.com/hyunwoongko/transformer/blob/master/conf.py
 """
 
 import torch
@@ -11,13 +12,15 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 num_layers = 6
 d_model = 512
 num_heads = 8
-d_ffn = 2048
+d_hidden = 2048
 drop_rate = 0.1
 batch_size = 128
 max_seq_len = 256
 
-# Optimizer hyperparameter
+# Optimizer hyperparameter (Adam optimizer)
 beta1 = 0.9
 beta2 = 0.98
 eps = 1e-9
 warmup_steps = 4000
+init_lr = 1e-5
+weight_decay = 5e-4

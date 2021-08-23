350ccf7065a295feff1f59961de0b2f591a4ccfa
mooncy0421 <co1504@naver.com>
2021-08-23 17:34:19 +0900

    FEAT: Add lr_scheduler and loss function
    Add learning rate scheduler part and loss function (Cross Entropy) part.

--- train.py
+++ train.py
@@ -12,6 +12,12 @@ from util.dataloader import DataLoader
 
 import torchtext
 
+
+def init_weights(model):
+    if hasattr(model, 'weight') and model.weight.dim() > 1:
+        nn.init.kaiming_uniform(model.weight.data)
+
+# Multi30k dataset
 loader = DataLoader(SRC_LANGUAGE='en',
                     TGT_LANGUAGE='de')
 
@@ -21,6 +27,7 @@ tgt_lang = 'de'
 train_data, valide_data, test_data = loader.load_dataset()
 loader.build_vocab(dataset=train_data, min_freq=2)
 
+# Transformer architecture
 model = Transformer(pad = loader.PAD_IDX,
                     enc_voc_size = len(loader.vocabs[src_lang]),
                     dec_voc_size = len(loader.vocabs[tgt_lang]),
@@ -32,8 +39,20 @@ model = Transformer(pad = loader.PAD_IDX,
                     n_layers = num_layers,
                     device = device)
 
+# Adam optimizer
 optimizer = Adam(params = model.parameters(),
                  lr = init_lr,
                  betas = (beta1, beta2),
                  eps = eps,
                  weight_decay = weight_decay)
+
+model.apply(init_weights)
+
+# Learning rate scheduler
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer = optimizer,
+                                                 verbose = True, 
+                                                factor = lr_factor,
+                                                patience = lr_patience)
+
+# Loss function (Cross Entropy)
+criterion = nn.CrossEntropyLoss(ignore_index=loader.PAD_IDX)

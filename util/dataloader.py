c5612680ed09e9b1d6cea437a983e33bd7f2725d
mooncy0421 <co1504@naver.com>
2021-08-26 09:55:29 +0900

    FEAT: Add method for making dataset iterator
    Add make_batched_iter and inner method collate_batch.
    The method 'collate_batch' tokenizes, numerizes and makes them tensors.
    The method 'make_batched_iter' uses 'collate_batch' to perform sequence
    to tensor transformation.

--- util/dataloader.py
+++ util/dataloader.py
@@ -4,6 +4,10 @@ Date : 2021-08-19
 Reference : https://pytorch.org/tutorials/beginner/translation_transformer.html
 """
 
+import torch
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
+
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torchtext.datasets import Multi30k
@@ -17,7 +21,7 @@ Dependency
     > python -m spacy download de_core_news_sm
 """
 
-class DataLoader:
+class TranslationDataloader:
     def __init__(self, SRC_LANGUAGE, TGT_LANGUAGE):
         # SRC_LANGUAGE and TGT_LANGUAGE must be 'de' or 'en'
         self.src_lan = SRC_LANGUAGE
@@ -66,3 +70,45 @@ class DataLoader:
         self.vocabs[self.tgt_lan].set_default_index(self.UNK_IDX)
 
         
+   # def make_batched_iter(self, dataset, batch_size):
+    #    def collate_batch(batch):
+     #       src_batch, tgt_batch = [], []
+      #      for src_sample, tgt_sample in batch:
+       #         src_batch.append()
+
+
+
+    def sequential_transforms(*transforms):
+        def func(txt_input):
+            for transform in transforms:
+                txt_input = transform(txt_input)
+            return txt_input
+        return func
+    
+    # tokens -> BOS + tokens + EOS
+    def tensor_transform(self, token_ids: List[int]):
+        return torch.cat((torch.tensor([self.BOS_IDX]),
+                          torch.tensor(token_ids),
+                          torch.tensor([self.EOS_IDX])))
+
+    
+
+    # token_transform : tokenizers
+    # vocab_transform : vocabs
+    # text_transform  : sequence를 tokenizer로 tokenize 후 vocab으로 만든 다음 BOS/EOS 더해 tensor화
+    def make_batched_iter(self, batch):
+        text_transform = {}
+        text_transform[self.src_lan] = self.sequential_transforms(self.tokenizers[self.src_lan],
+                                                                  self.vocabs[self.src_lan],
+                                                                  self.tensor_transform)
+        def collate_batch(batch):
+            src_batch, tgt_batch = [], []
+            for src_sample, tgt_sample in batch:
+                src_batch.append(text_transform[self.src_lan](src_sample.rstrip("\n")))
+                tgt_batch.append(text_transform[self.tgt_lan](tgt_sample.rstrip("\n")))
+
+            src_batch = pad_sequence(sequences=src_batch, padding_value=self.PAD_IDX)
+            tgt_batch = pad_sequence(sequences=tgt_batch, padding_value=self.PAD_IDX)
+            return src_batch, tgt_batch
+
+        batched_iter = DataLoader()
